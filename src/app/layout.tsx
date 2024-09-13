96b02c6e53e720fa4f1471c44c6dd7dd92fece40
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2024-09-14 02:19:30 +0530

    chore: added vercel SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { App } from "./app";
 import Head from "next/head";
 import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           {children}
           <Analytics />
           <Toaster />
+          <SpeedInsights />
         </body>
       </html>
     </App>
